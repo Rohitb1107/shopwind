ef3cccf076ab0ed23a8b08b0ec9f6aaba3227fd5
Rohit <stylishrb711@gmail.com>
2022-07-22 10:44:50 +0530

    server created

---
+++ client/src/Components/AllRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Navbar from "./Navbar/Navbar";
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route to="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
